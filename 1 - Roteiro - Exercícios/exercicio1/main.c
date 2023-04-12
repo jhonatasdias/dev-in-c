8fda7821308fc7f4925dae4c9d3b74618d91e9da
Jhonata Dias <118060258+jhonstudentx@users.noreply.github.com>
2023-04-11 22:16:57 -0300

    Update main.c

--- 1 - Roteiro - Exercícios/exercicio1/main.c
+++ 1 - Roteiro - Exercícios/exercicio1/main.c
@@ -5,12 +5,16 @@ int main (void)
 {
     int x, y = 0;
     int subt = 0;
+    
     printf("Subtrair o primeiro pelo segundo.\n");
     printf("Digite o primeiro valor: ");
-    scanf("%d",&x);
+    scanf("%d", &x);
     printf("Digite o segundo valor: ");
-    scanf("%d",&y);
+    scanf("%d", &y);
+    
     subt = x - y;
+    
     printf("A subtracao do primeiro pelo segundo e: %d \n", subt);
+    
     return (0);
 }
